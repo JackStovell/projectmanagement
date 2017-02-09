f0106471537fb56b766a31fd6bc97df60e536832
Jack Stovell <jackastovell@gmail.com>
2017-02-09 17:25:25 +0000

    working appgit add --allgit add --all

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  resources :tasks
   devise_for :users
- 
-  root 'pages#home'
-  
-  get 'about' => 'pages#about'
+
+  resources :tasks do
+    member do
+      post :change
+    end
+  end
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#home'
+
+  get 'about' => "pages#about"
+  get 'test' => "pages#test"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
